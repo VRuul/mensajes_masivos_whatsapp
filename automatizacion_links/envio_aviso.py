c9667e7086a8948d7a8b011ccaafa73ba8ccfec2
VRuul <victorraulortizgarcia@gamil.com>
2023-11-13 11:03:13 -0600

    actualizando codigo del archivo encio aviso, para reenviar pre invitacion

--- automatizacion_links/envio_aviso.py
+++ automatizacion_links/envio_aviso.py
@@ -9,10 +9,10 @@ from tkinter import messagebox
 df = pd.read_csv('csv/prueba.csv')
 
 # Texto que deseas agregar antes del enlace
-# texto_adicional = "Les compartimos el enlace de la pre-invitación de los XV años de nuestra hija Elisabet Ortiz Garcia. "
+texto_adicional = "Buenos días, se les invita a confirmar su asistencia a la fiesta de XV años de nuestra hija Elisabet, ya que se realizará el cierre de la lista de invitados. Si no pueden abrir el enlace, favor de copiarlo y pegarlo en el navegador de su preferencia. Por su atención, gracias.  "
 # otro_texto = " Atentamente, Familia Ortiz Garcia."
 #Re-envio
-texto_adicional = "Buenos días, se les hace él reenvió de la preinvitación de nuestra hija Elisabet."
+# texto_adicional = "Buenos días, se les hace él reenvió de la preinvitación de nuestra hija Elisabet."
 
 # Archivo de registro
 archivo_de_registro = open('registro.txt', 'w')
@@ -25,14 +25,14 @@ for index, row in df.iterrows():
     # Construir el mensaje
     # mensaje = f"{texto_adicional}{enlace}{otro_texto}"
     # mensaje2 = f"{enlace}"
-    mensaje = f"{texto_adicional}"
+    mensaje = f"{texto_adicional}{enlace}"
     mensaje2 = f"{enlace}"
 
     try:
         # Enviar el mensaje a través de WhatsApp
-        kit.sendwhatmsg_instantly(f'+{numero}', mensaje, 17, True, 4)
+        kit.sendwhatmsg_instantly(f'+{numero}', mensaje, 15, True, 4)
         print(f"Mensaje enviado a {numero}")
-        kit.sendwhatmsg_instantly(f'+{numero}', mensaje2, 15, True, 4)
+        # kit.sendwhatmsg_instantly(f'+{numero}', mensaje2, 17, True, 4)
     except Exception as e:
         error_msg = f"No se pudo enviar el mensaje a {numero}: {str(e)}"
         print(error_msg)
